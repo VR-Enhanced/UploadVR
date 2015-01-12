bd2f7407f345eb785ef6bf5beebe6e717959f59f
Eric <Ericrius1@gmail.com>
2015-01-11 18:11:50 -0800

    multiple posts

--- js/posts.js
+++ js/posts.js
@@ -1,4 +1,5 @@
 function Posts(){
+  var panels = [];
   $.ajax({
 
     url: 'posts/posts.json',
@@ -9,14 +10,23 @@ function Posts(){
       console.log(errorThrown)
     },
     success: function(data){
-      this.createPanel(data.post, new THREE.Vector3(0, 100, -500));
-      this.createPanel(data.post, new THREE.Vector3(300, 100, -500));
+      this.createPanels(data.posts, new THREE.Vector3(0, 100, -500));
     }.bind(this)
   });
 
 }
 
-Posts.prototype.createPanel = function(post, position){
+Posts.prototype.createPanels = function(posts){
+    var scale = 8;
+    var geo = new THREE.PlaneBufferGeometry(200, 200)
+    var mat = new THREE.MeshBasicMaterial({
+      color: 0xff00ff,
+      transparent: true,
+      opacity: 0.7
+    });
+
+  for(var i = 0; i < posts.length; i++){
+    var post = posts[i];
     var $post = $('<div class = "post"><div class = "title"></div><div class="content"></div></div>');
     $post.find('.title').text(post.title);
     var $content = $post.find('.content');
@@ -25,32 +35,24 @@ Posts.prototype.createPanel = function(post, position){
       $paragraph.text(paragraph);
       $content.append($paragraph)
     });
+    
+    var panelContainer = new THREE.Mesh(geo, mat);
+    panelContainer.position.set(i * 300, 100, -500)
+    scene.add(panelContainer);
 
+    var panel = new THREE.Object3D();
+    panelContainer.add(panel);
+    panel.html = $post[0];
+    panel.content = new THREE.CSS3DObject(panel.html);
+    panel.content.scale.set(0.2, 0.2, .2);
+    panel.add(panel.content);
+  }
 
-    var scale = 8;
-    var mat = new THREE.MeshBasicMaterial({
-      color: 0xff00ff,
-      transparent: true,
-      opacity: 0.7
-    })
-    this.panelContainer = new THREE.Mesh(new THREE.PlaneBufferGeometry(200, 200), mat);
-    this.panelContainer.position.copy(position)
-    scene.add(this.panelContainer);
-
-    this.panel = new THREE.Object3D();
-    this.panelContainer.add(this.panel);
-    this.panel.html = $post[0];
-    this.panel.content = new THREE.CSS3DObject(this.panel.html);
-    this.panel.content.scale.set(0.2, 0.2, .2);
-    this.panel.add(this.panel.content);
-
-    this.created = true;
 
 }
 
+
 Posts.prototype.update = function(){
-  if(!this.created){
-    return;
-  }
+
 
 }
