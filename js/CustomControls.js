5cd23ad96374d380371dd4603eabcd34299ed929
Eric <Ericrius1@gmail.com>
2015-01-13 23:32:32 -0800

    apost

--- js/CustomControls.js
+++ js/CustomControls.js
@@ -2,7 +2,7 @@ var CustomControls;
 
 CustomControls = (function() {
   function CustomControls() {
-    G.oculusControls = true;
+    G.oculusControls = false;
     if (G.oculusControls) {
       G.controls = new THREE.OculusRiftControls(camera);
     }else {
