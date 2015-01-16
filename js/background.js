80ed1b7cfe80e125d81e5f57f480047734cf816c
Eric <Ericrius1@gmail.com>
2015-01-16 15:06:50 -0800

    logo

--- js/background.js
+++ js/background.js
@@ -53,7 +53,7 @@ Background.prototype.generateLogo = function() {
   this.logoGroup = new SPE.Group({
     texture: texture,
     maxAge: 3,
-    blending: THREE.NormalBlending
+    // blending: THREE.NormalBlending
   });
   var textGeo = new THREE.TextGeometry("U  P  L  O  A  D     V  R", {
     size: 10,
@@ -61,7 +61,7 @@ Background.prototype.generateLogo = function() {
     height: 1
   });
   this.logoContainer = new THREE.Object3D();
-  this.logoContainer.position.set(-50, 70, G.userStartZ - 200)
+  this.logoContainer.position.set(-55, -20, G.userStartZ - 100)
   scene.add(this.logoContainer);
   this.logoMesh = new THREE.Mesh(textGeo);
   this.logoContainer.add(this.logoMesh);
@@ -72,11 +72,33 @@ Background.prototype.generateLogo = function() {
 
   this.logoContainer.remove(this.logoMesh);
 
+  var i = {
+    y: this.logoContainer.position.y
+  };
+  var f = {
+    y:  60
+  }
+
+
+  setTimeout(function() {
+    var logoTween = new TWEEN.Tween(i).
+    to(f, 5000).
+    onUpdate(function() {
+      this.logoContainer.position.y = i.y
+    }.bind(this)).start();
+
+    logoTween.onComplete(function() {
+      for(var i  =0; i < this.emitters.length; i++){
+        this.emitters[i].disable()
+      }
+    }.bind(this));
+  }.bind(this), 2000);
+
 }
 
 Background.prototype.createEmitterPoints = function(points) {
   var emitter;
-  for(var i =0; i < points.length; i++){
+  for (var i = 0; i < points.length; i++) {
     emitter = new SPE.Emitter(this.emitterParams);
     emitter.position = points[i];
     this.logoGroup.addEmitter(emitter);
